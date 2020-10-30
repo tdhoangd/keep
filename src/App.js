f7528e02425d6bda8d66aed55d949924d9a6183f
Thanh Hoang <thanhhoangss@gmail.com>
2020-10-29 19:32:47 -0700

    no message

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import CreateNote from "./component/create-note/create-note";
 
 class App extends Component {
   render() {
-    console.log(process.env.KEEP_APP_FB_API_KEY);
+    console.log(process.env.KEEP_APP_FB_AUTH);
 
     return (
       <div className={'App'}>
